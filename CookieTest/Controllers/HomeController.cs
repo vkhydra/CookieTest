feb3b6d34d5e0a698c6569fd8c25f1f1a6bcfb58
Victor Bruno Kulessa <victor.kulessa@gmail.com>
2022-01-03 10:17:54 -0300

    Add project files.

---
+++ CookieTest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using CookieTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace CookieTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var visitString = Request.Cookies["visits"];
+            int visits = 0;
+            int.TryParse(visitString, out visits);
+            visits++;
+            CookieOptions options = new();
+            options.Expires = DateTimeOffset.Now.AddSeconds(20);
+            Response.Cookies.Append("visits", visits.ToString(), options);
+
+            //Response.Cookies.Delete("visits");
+
+            ViewBag.visits = visits;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
